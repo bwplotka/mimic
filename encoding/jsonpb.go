e3194581d549b623fe74a4e3a77dfb4e4cb4b6ec
Dominic Green <dominicgreen1@gmail.com>
2019-07-05 15:16:23 +0800

    split encoding files ... need to fix imports

---
+++ encoding/jsonpb.go
@@ -0,0 +1,25 @@
+package encoding
+
+import (
+	"bytes"
+	"io"
+)
+
+// JSONPB returns reader that encodes protobuf messages to JSON.
+// NOTE: The jsonpb marshaler behaves slightly differently to go's built in marshaler.
+func JSONPB(in proto.Message) io.Reader {
+	str, err := (&jsonpb.Marshaler{Indent: "  "}).MarshalToString(in.(proto.Message))
+	if err != nil {
+		return errReader{err: errors.Wrapf(err, "unable to marshal protobuf to JSONPB: %v", in)}
+	}
+	return bytes.NewBufferString(str)
+}
+
+// GogoJSONPB returns reader that encodes protobuf messages built with gogo/protobuf implementation.
+func GogoJSONPB(in protogg.Message) io.Reader {
+	str, err := (&jsonggpb.Marshaler{Indent: " "}).MarshalToString(in.(protogg.Message))
+	if err != nil {
+		return errReader{err: errors.Wrapf(err, "unable to marshal protobuf to GogoJSONPB: %v", in)}
+	}
+	return bytes.NewBufferString(str)
+}
---
+++ encoding/yaml.go
@@ -0,0 +1,54 @@
+package encoding
+
+import (
+	"bytes"
+	"errors"
+	"io"
+)
+
+// GhodssYAML returns reader that encodes anything to YAML using github.com/ghodss/yaml.
+// Desired for e.g:
+// * Kubernetes
+func GhodssYAML(in ...interface{}) io.Reader {
+	return yaml(ghodssyaml.Marshal, in...)
+}
+
+// YAML returns reader that encodes anything to YAML using gopkg.in/yaml.v2.
+// Desired for e.g:
+// * Prometheus, Alertmanager configuration
+func YAML(in ...interface{}) io.Reader {
+	return yaml(yaml2.Marshal, in...)
+}
+
+type MarshalFunc func(o interface{}) ([]byte, error)
+
+func yaml(marshalFn MarshalFunc, in ...interface{}) io.Reader {
+	var concatDelim = []byte("---\n")
+
+	if len(in) == 0 {
+		return errReader{err: errors.New("Nothing to output")}
+	}
+	var res [][]byte
+	for _, entry := range in {
+		var entryBytes []byte
+
+		// Do not marshal strings - they should be appended directly
+		if extraString, ok := entry.(string); ok {
+			entryBytes = []byte(extraString)
+		} else {
+			b, err := marshalFn(entry)
+			if err != nil {
+				return errReader{err: errors.Wrapf(err, "unable to marshal to YAML: %v", in)}
+			}
+			entryBytes = b
+		}
+		res = append(res, entryBytes)
+	}
+
+	if len(res) == 1 {
+		return bytes.NewBuffer(res[0])
+	}
+
+	return bytes.NewBuffer(bytes.Join(res, concatDelim))
+}
+
